3e4698da5813d49b7e10ca19bb7fde38f3a477da
Ida Darmochwał <ida.mdart@gmail.com>
2022-03-19 23:31:27 +0100

    Modify Stopwatch and StopwatchWithResults classes

--- src/StopwatchWithResults.ts
+++ src/StopwatchWithResults.ts
@@ -1,48 +1,45 @@
-import Stopwatch from './Stopwatch.js'
+import Stopwatch from './Stopwatch.js';
 
 class StopwatchWithResults extends Stopwatch {
-  
-  results = []
 
-  constructor(element) {
-    super(element)
-    this.prepareElements(element)
-    this.prepareActions()
+  private results: Array<string> = [];
+
+  constructor(element: HTMLDivElement) {
+    super(element);
+    this.prepareElements(element);
+    this.prepareActions();
   }
 
-  prepareElements(element) {
-    this.dom.resultsList = element.querySelector('.stopwatch__results')
-    this.dom.addToListBtn = element.querySelector('.stopwatch__start-add-to-list')
-    this.dom.resetListBtn = element.querySelector('.stopwatch__start-reset-list')
+  private prepareElements(element: HTMLDivElement) {
+    this.dom.resultsList = <HTMLElement> element.querySelector('.stopwatch__results .stopwatch__results__list');
+    this.dom.addToListBtn = <HTMLButtonElement> element.querySelector('.stopwatch__add-to-list-btn');
+    this.dom.resetListBtn = <HTMLButtonElement> element.querySelector('.stopwatch__reset-list-btn');
   }
 
-  prepareActions() {
-    /*
-    Funkcja ta powinna dodawać nasłuchwiacze do buttonów this.dom.addToListBtn oraz this.dom.resetListBtn.
-    Pierwszy powinien po kliknięciu uruchamiać metodę this.addToList, a druga this.resetList.
-    */
+  private prepareActions(): void {
+    this.dom.addToListBtn.addEventListener('click', () => this.addToList());
+    this.dom.resetListBtn.addEventListener('click', () => this.resetList());
   }
 
-  renderList() {
-    /*
-    Funkcja ta powinna czyścić zawartość this.dom.resultsList, a następnie renderować w niej nowe elementy li
-    na podstawie zawartości tablicy this.results. Każdy jej element powinien być renderowany bez żadnych zmian.
+  private renderList(): void {
+    this.dom.resultsList?.replaceChildren();
 
-    np. <li>00:12:00</li>
-    */
+    (this.results.length > 0) && this.results.map(el => {
+      this.dom.resultsList.insertAdjacentHTML('beforeend', `<li>${el}</li>`);
+    })
   }
 
-  addToList() {
-    /*
-    Funkcja ta powinna pobierać aktualny czas z this.currentTime, formatować go i w takiej postaci zapisywać do tablicy this.results.
-    Następnie powinna renderować aktualną listę na stronie (this.renderList).
-    */
+  private addToList(): void {
+    const currTime: string = this.formatTime(this.currentTime);
+
+    if(this.results.includes(currTime) === false) this.results.push(currTime);
+    this.renderList();
   }
 
-  resetList() {
-    /*
-    Funkcja ta powinna czyścić tablicę this.results oraz zawartość this.dom.resultsList
-    */
+  private resetList(): void {
+    this.results = [];
+    this.dom.resultsList?.replaceChildren();
+    this.dom.resultsList.insertAdjacentHTML('beforeend', `<li>No results :(</li>`);
   }
 
 }
